8a866008be05afbd4f760792fe55464dd85e81f1
Eevan <eevan.redon@gmail.com>
2020-02-11 00:00:38 +0100

    little hotel card styling

--- src/components/HotelsList.jsx
+++ src/components/HotelsList.jsx
@@ -14,35 +14,39 @@ const HotelsList = props => {
   };
 
   let hotelCard;
-  
+
   if (gotHotelsData == true) {
     hotelCard = props.hotels.results.map(hotel => {
-        return (
-          <div className="ui card">
-            <div class="image"><img src="https://thumbnails.trvl-media.com/PUrr-BSAcHRWzkWDuOP2XTmK80I=/773x530/smart/filters:quality(60)/images.trvl-media.com/hotels/1000000/600000/598500/598487/30a71d36_z.jpg" /></div>
+      return (
+        <div className="centerText">
+          <div id="hotel-cards" className="ui card">
+            <div className="image"><img src="https://thumbnails.trvl-media.com/PUrr-BSAcHRWzkWDuOP2XTmK80I=/773x530/smart/filters:quality(60)/images.trvl-media.com/hotels/1000000/600000/598500/598487/30a71d36_z.jpg" /></div>
             <div className="content">
-              <div class="header">{hotel.name}</div>
-              <div class="description">
+              <div className="header">{hotel.name}</div>
+              <div className="description">
                 {hotel.address}
                 <br />
                 {hotel.url}
               </div>
             </div>
             <div className="extra content">
-              {hotel.price} SEK / Night
-            </div>
+              We found deals from {hotel.price} SEK / Night
+              </div>
           </div>
+        </div>
         );
     });
   }
 
 
-  return (
-    <>
-      <Button id="get-hotels" onClick={getHotelsShowData}>Show Hotels</Button>
+return (
+  <>
+    <Button id="get-hotels" onClick={getHotelsShowData}>Show Hotels</Button>
+    <div className="ui stackable four column grid">
       {hotelCard}
-    </>
-  )
+    </div>
+  </>
+)
 }
 
 const mapStateToProps = state => {
