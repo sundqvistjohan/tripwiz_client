bcafe335d211b9245f40f7141fd06ed9e4b7bcfe
Eevan <eevan.redon@gmail.com>
2020-02-11 10:07:56 +0100

    adds info window for markers

--- src/components/HotelsList.jsx
+++ src/components/HotelsList.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { getHotels } from "../modules/destination.js";
-import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+import { Map, InfoWindow, GoogleApiWrapper, Marker } from "google-maps-react";
+import { Button } from "semantic-ui-react";
 
 
 const HotelsList = props => {
   const [gotHotelsData, setGotHotelsData] = useState(false);
-
+  const [showingInfoWindow, setShowingInfoWindow] = useState(false);
+  const [activeMarker, setActiveMarker] = useState({});
+  const [selectedPlace, setSelectedPlaces] = useState({});
 
   const getHotelsShowData = async () => {
     const hotelsData = await getHotels(
@@ -32,7 +35,9 @@ const HotelsList = props => {
           <div id="hotel-cards" className="ui card">
             <div className="image"><img src="https://thumbnails.trvl-media.com/PUrr-BSAcHRWzkWDuOP2XTmK80I=/773x530/smart/filters:quality(60)/images.trvl-media.com/hotels/1000000/600000/598500/598487/30a71d36_z.jpg" /></div>
             <div className="content">
-              <div className="header">{hotel.name}</div>
+              <div className="header">
+                {hotel.name}
+              </div>
               <div className="description">
                 {hotel.address}
                 <br />
@@ -40,7 +45,10 @@ const HotelsList = props => {
               </div>
             </div>
             <div className="extra content">
-              Current deals from {hotel.price} SEK / Night
+              <div id="price-box">
+                Current deals from {hotel.price} SEK / Night
+              </div>
+              <Button>Add to Itinerary</Button>
             </div>
           </div>
         </div>
@@ -48,6 +56,11 @@ const HotelsList = props => {
     });
   }
 
+  const onMarkerClick = (props, marker, e) => {
+    setSelectedPlaces(props)
+    setActiveMarker(marker)
+    setShowingInfoWindow(true)
+  };
 
   return (
     <>
@@ -60,30 +73,35 @@ const HotelsList = props => {
         {hotelCard}
       </div>
       <div id="embed-map">
-      {hotelCard ? (
-        <div className="hotels-map">
-          <Map google={props.google}
-            style={{ width: '90%', height: '60%', position: 'relative' }}
-            className={'map'}
-            zoom={14}
-            initialCenter={{
-              lat: props.hotels[0].lat,
-              lng: props.hotels[0].lng
-            }}>
-            <Marker
-              name={props.hotels[0].name}
-              position={{ lat: props.hotels[0].lat, lng: props.hotels[0].lng }} />
-            <Marker
-              name={props.hotels[1].name}
-              position={{ lat: props.hotels[1].lat, lng: props.hotels[1].lng }} />
-            <Marker />
-            <Marker
-              name={props.hotels[2].name}
-              position={{ lat: props.hotels[2].lat, lng: props.hotels[2].lng}} />
-            <Marker />
-          </Map>
-        </div>
-      ) : null}
+        {hotelCard ? (
+          <div className="hotels-map">
+            <Map google={props.google}
+              style={{ width: '90%', height: '60%', position: 'relative' }}
+              className={'map'}
+              zoom={14}
+              initialCenter={{
+                lat: props.hotels[0].lat,
+                lng: props.hotels[0].lng
+              }}>
+              <Marker onClick={onMarkerClick}
+                name={props.hotels[0].name}
+                position={{ lat: props.hotels[0].lat, lng: props.hotels[0].lng }} />
+              <Marker onClick={onMarkerClick}
+                name={props.hotels[1].name}
+                position={{ lat: props.hotels[1].lat, lng: props.hotels[1].lng }} />
+              <Marker onClick={onMarkerClick}
+                name={props.hotels[2].name}
+                position={{ lat: props.hotels[2].lat, lng: props.hotels[2].lng }} />
+              <InfoWindow
+                marker={activeMarker}
+                visible={showingInfoWindow}>
+                <div>
+                  <h4>{selectedPlace.name}</h4>
+                </div>
+              </InfoWindow>
+            </Map>
+          </div>
+        ) : null}
       </div>
     </>
   )
