65a56d00f3877633f42f7de93f567b8116a5b41a
Johan Sundqvist <sundqvist.johan@outlook.com>
2020-02-14 17:41:02 +0100

    Adds respone to auth stub

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -110,14 +110,19 @@ Cypress.Commands.add("login", () => {
       url: "v3.1/me?access_token*",
       status: "200",
       response: {
-        errors: ["Invalid login credentials. Please try again."],
-        success: false
+        data: ["nice"],
+        success: true
       }
     });
     cy.route({
       method: "POST",
-      url: "/auth",
+      url: "**auth**",
       status: "200",
+      response: {
+        data: ["nice"],
+        success: true
+      }
     });
     cy.get("#login-button").click();
+    cy.get("h2").should("contain", "Let's get started")
 })
