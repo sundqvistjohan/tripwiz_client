4ac619a57a1235cd04e3bcac35cfd9a0807f4bb5
Eevan <eevan.redon@gmail.com>
2020-02-24 19:18:34 +0100

    sad path for user refreshing page, show login based on presence of local storage token

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,29 @@ import React from 'react';
 import { connect } from "react-redux";
 
 const Header = (props) => {
-  debugger
+
+  let loggedIn;
+  switch (true) {
+    case props.authenticated:
+      loggedIn = <h5>Logged in as {props.current_user.name}</h5>
+      break;
+    case !props.authenticated && localStorage.getItem("J-sunkAuth-Storage") !== null:
+      loggedIn = <h5>Logged in with <span style={{color: '#4267b2'}}>Facebook</span></h5>
+      break;
+  }
+
   return (
     <>
       <header className="ui fixed inverted menu">
         <div className="inner">
-          <a href="/"><h5 className="masthead-brand">TripWIZ</h5></a>
+          <a href="/"><h5 className="masthead-brand">
+            <span style={{ color: 'rgb(82, 80, 80)' }}>
+              TripWIZ
+            </span>
+          </h5></a>
         </div>
         <div className="right menu">
-          {props.authenticated === true && (
-          <h5>Logged in as {props.current_user.name}</h5> 
-          )}
+          {loggedIn}
         </div>
       </header>
     </>
@@ -20,10 +32,10 @@ const Header = (props) => {
 }
 
 const mapStateToProps = state => {
-	return {
+  return {
     current_user: state.current_user,
     authenticated: state.authenticated
-	};
+  };
 };
 
 export default connect(
