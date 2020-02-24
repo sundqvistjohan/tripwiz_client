b2bb7e1da5ddce64976c5ea7953d90a57c3be179
Eevan <eevan.redon@gmail.com>
2020-02-24 21:15:53 +0100

    adds process.env back to axios base URL

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ import { Provider } from "react-redux";
 import { createStore } from 'redux';
 import rootReducer from './state/reducers/rootReducer'
 
-axios.defaults.baseURL = 'http://localhost:3000';
-// process.env.REACT_APP_API_BASEURL
+axios.defaults.baseURL = process.env.REACT_APP_API_BASEURL;
 
 const store = createStore(rootReducer);
 
