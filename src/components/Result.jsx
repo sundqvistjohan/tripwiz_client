5d56bef3517e0ef1a1b930d5e0fe73e873900c8e
mantanquzzmo <quzzmo@hotmail.com>
2020-03-01 23:55:50 +0100

    adds correct conditional when no trips availible

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -17,7 +17,6 @@ import { Redirect } from "react-router";
 const Result = props => {
   const [redirect, setRedirect] = useState(false);
   const [loading, setLoading] = useState("hidden");
-  debugger
 
   const panes = [
     {
@@ -67,7 +66,6 @@ const Result = props => {
   const setActivities = async () => {
     let response = await getActivities(props.trip);
     props.setActivities(response.data);
-    debugger
   };
 
   const setRestaurants = async () => {
