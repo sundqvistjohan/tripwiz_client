38161b93d6c2c8b2ff7c106816261713c889e010
mantanquzzmo <quzzmo@hotmail.com>
2020-04-20 14:24:43 -0400

    shrinks the result screen

--- src/components/Ranking.jsx
+++ src/components/Ranking.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { getHighScoreData } from "../modules/destination.js";
+import { Button } from "semantic-ui-react"
 
 const Ranking = () => {
   
   const [highScoreData, setHighScoreData] = useState(null);
+  const [currentRanking, setCurrentRanking] = useState("destination")
 
   const getHighScore = async () => {
     let response = await getHighScoreData();
@@ -20,103 +22,34 @@ const Ranking = () => {
 
   return (
     <div className="highscore-div-main">
+      <div classname="ranking-buttons">
       <h5>Top rated destination by:</h5>
-      {highScoreData && highScoreData["destination"][0] && (
+      <Button onClick={() => setCurrentRanking("destination")}>Destination</Button>
+      <Button onClick={() => setCurrentRanking("activities")}>Activities</Button>
+      <Button onClick={() => setCurrentRanking("restaurants")}>Restaurants</Button>
+      <Button onClick={() => setCurrentRanking("hotel")}>Hotel</Button>
+      </div>
+      {highScoreData && highScoreData[currentRanking][0] && (
         <div className="highscore-div">
           <h5>Destination!</h5>
-          <p>{highScoreData["destination"][0].destination}</p>
-          <p>{highScoreData["destination"][0].rating}</p> <br />
-          <p>
-            {highScoreData["destination"][1] &&
-              highScoreData["destination"][1].destination}
+          <p>1. {highScoreData[currentRanking][0].destination}</p>
+          <p>Score: {highScoreData[currentRanking][0].rating}</p>
+          <p>2. {" "}
+            {highScoreData[currentRanking][1] &&
+              highScoreData[currentRanking][1].destination}
           </p>
-          <p>
-            {highScoreData["destination"][1] &&
-              highScoreData["destination"][1].rating}
+          <p>Score: {" "}
+            {highScoreData[currentRanking][1] &&
+              highScoreData[currentRanking][1].rating}
           </p>{" "}
-          <br />
           <p>
-            {highScoreData["destination"][2] &&
-              highScoreData["destination"][2].destination}
+            {highScoreData[currentRanking][2] &&
+              highScoreData[currentRanking][2].destination}
           </p>
           <p>
-            {highScoreData["destination"][2] &&
-              highScoreData["destination"][2].rating}
+            {highScoreData[currentRanking][2] &&
+              highScoreData[currentRanking][2].rating}
           </p>{" "}
-          <br />
-        </div>
-      )}
-      {highScoreData && highScoreData["activities"][0] && (
-        <div className="highscore-div">
-          <h5>Activities!</h5>
-          <p>
-            {highScoreData["activities"][0].destination}{" "}
-            {highScoreData.activities[0].activity}
-          </p>
-          <p>{highScoreData["activities"][0].rating}</p> <br />
-          <p>
-            {highScoreData["activities"][1] &&
-              highScoreData["activities"][1].destination}{" "}
-            {highScoreData["activities"][1] &&
-              highScoreData["activities"][1].activity}
-          </p>
-          <p>
-            {highScoreData["activities"][1] &&
-              highScoreData["activities"][1].rating}
-          </p>{" "}
-          <br />
-          <p>
-            {highScoreData["activities"][2] &&
-              highScoreData["activities"][2].destination}{" "}
-            {highScoreData["activities"][2] &&
-              highScoreData["activities"][2].activity}
-          </p>
-          <p>
-            {highScoreData["activities"][2] &&
-              highScoreData["activities"][2].rating}
-          </p>{" "}
-          <br />
-        </div>
-      )}
-      {highScoreData && highScoreData["restaurants"][0] && (
-        <div className="highscore-div">
-          <h5>Restaurants!</h5>
-          <p>{highScoreData["restaurants"][0].destination}</p>
-          <p>{highScoreData["restaurants"][0].rating}</p> <br />
-          <p>
-            {highScoreData["restaurants"][1] &&
-              highScoreData["restaurants"][1].destination}
-          </p>
-          <p>
-            {highScoreData["restaurants"][1] &&
-              highScoreData["restaurants"][1].rating}
-          </p>{" "}
-          <br />
-          <p>
-            {highScoreData["restaurants"][2] &&
-              highScoreData["restaurants"][2].destination}
-          </p>s
-          <p>
-            {highScoreData["restaurants"][2] &&
-              highScoreData["restaurants"][2].rating}
-          </p>{" "}
-          <br />
-        </div>
-      )}
-      {highScoreData && highScoreData["hotel"][0] && (
-        <div className="highscore-div">
-          <h5>Hotel!</h5>
-          <p>{highScoreData["hotel"][0].destination}</p>
-          <p>{highScoreData["hotel"][0].rating}</p> <br />
-          <p>
-            {highScoreData["hotel"][1] && highScoreData["hotel"][1].destination}
-          </p>
-          <p>{highScoreData["hotel"][1] && highScoreData["hotel"][1].rating}</p>{" "}
-          <br />
-          <p>
-            {highScoreData["hotel"][2] && highScoreData["hotel"][2].destination}
-          </p>
-          <p>{highScoreData["hotel"][2] && highScoreData["hotel"][2].rating}</p>{" "}
           <br />
         </div>
       )}
