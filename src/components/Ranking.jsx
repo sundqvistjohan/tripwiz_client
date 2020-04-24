c010f75a33ed441b47cbc7c34fb445bf50766c8e
mantanquzzmo <quzzmo@hotmail.com>
2020-04-24 16:58:06 -0400

    styles component

--- src/components/Ranking.jsx
+++ src/components/Ranking.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { getHighScoreData } from "../modules/destination.js";
 import { Button } from "semantic-ui-react";
-import { capitalize } from "../helpers/methods.js"
+import { capitalize } from "../helpers/methods.js";
 
 const Ranking = () => {
   const [highScoreData, setHighScoreData] = useState(null);
@@ -22,18 +22,18 @@ const Ranking = () => {
 
   return (
     <div className="highscore-div-main">
-      <div classname="ranking-buttons">
-        <h5>Top rated destination by:</h5>
-        <Button onClick={() => setCurrentRanking("destination")}>
+      <div className="ranking-buttons">
+        <h5 className="ranking-title">Top rated destination by:</h5>
+        <Button color='teal' onClick={() => setCurrentRanking("destination")}>
           Destination
         </Button>
-        <Button onClick={() => setCurrentRanking("activities")}>
+        <Button color='teal' onClick={() => setCurrentRanking("activities")}>
           Activities
         </Button>
-        <Button onClick={() => setCurrentRanking("restaurants")}>
+        <Button color='teal' onClick={() => setCurrentRanking("restaurants")}>
           Restaurants
         </Button>
-        <Button onClick={() => setCurrentRanking("hotel")}>Hotel</Button>
+        <Button color='teal' onClick={() => setCurrentRanking("hotel")}>Hotel</Button>
       </div>
       {highScoreData && highScoreData[currentRanking][0] && (
         <div className="highscore-div">
@@ -41,28 +41,25 @@ const Ranking = () => {
           <p>1. {highScoreData[currentRanking][0].destination}</p>
           <p>Rating: {highScoreData[currentRanking][0].rating}</p>
           <p>
-            2.{" "}
-            {highScoreData[currentRanking][1]
-              ? highScoreData[currentRanking][1].destination
-              : "N/A"}
+            {highScoreData[currentRanking][1] && "2. "}
+            {highScoreData[currentRanking][1] &&
+              highScoreData[currentRanking][1].destination}
+              
           </p>
           <p>
-            Rating:{" "}
-            {highScoreData[currentRanking][1]
-              ? highScoreData[currentRanking][1].rating
-              : "N/A"}
+            {highScoreData[currentRanking][1] && "Rating: "}
+            {highScoreData[currentRanking][1] &&
+              highScoreData[currentRanking][1].rating}
           </p>{" "}
           <p>
-            3.{" "}
-            {highScoreData[currentRanking][2]
-              ? highScoreData[currentRanking][2].destination
-              : "N/A"}
+            {highScoreData[currentRanking][2] && "3. "}
+            {highScoreData[currentRanking][2] &&
+              highScoreData[currentRanking][2].destination}
           </p>
           <p>
-            Rating:{" "}
-            {highScoreData[currentRanking][2]
-              ? highScoreData[currentRanking][2].rating
-              : "N/A"}
+            {highScoreData[currentRanking][2] && "Rating: "}
+            {highScoreData[currentRanking][2] &&
+              highScoreData[currentRanking][2].rating}
           </p>
           <br />
         </div>
